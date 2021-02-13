9c17797d8d51b2446a799808f403c0b806be59b5
ZerosAndOnes0101 <45151859+ZerosAndOnes0101@users.noreply.github.com>
2021-02-12 21:21:34 -0500

    Update SkystoneAutoMode.java

--- SkystoneAutoMode.java
+++ SkystoneAutoMode.java
@@ -1,7 +1,13 @@
 
-
 package org.firstinspires.ftc.teamcode ;
 
+
+import android.media.MediaPlayer;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.MechanumDrive;
+
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -15,6 +21,7 @@ import org.openftc.easyopencv.OpenCvInternalCamera;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -24,57 +31,123 @@ import static org.man.c.SkystoneDetector.Location.Two;
 import static org.man.c.SkystoneDetector.Location.Zero;
 
 
-@Autonomous(name="Skystone Detecotor", group="Auto")
+@Autonomous(name="SkystoneAutoMode", group="Auto")
 
-public class SkystoneAutoMode extends LinearOpMode {
+public class SkystoneAutoMode extends MechanumDrive {
 
-    private DcMotor test;
-
-
-
-
+    public DcMotor leftBackDrive = null;
+    public DcMotor rightBackDrive = null;
+    public DcMotor leftDrive = null;
+    public DcMotor rightDrive = null;
+    public Servo claw =  null;
+    public DcMotor arm = null;
+    public DistanceSensor x = null;
 
 
     OpenCvCamera phoneCam;
     @Override
-    public void runOpMode() throws InterruptedException {
-        double sp = 0;
-        test = hardwareMap.get(DcMotor.class, "te");
-        int cameraMonitorViewId = hardwareMap.appContext
-                .getResources().getIdentifier("cameraMonitorViewId",
-                        "id", hardwareMap.appContext.getPackageName());
-        phoneCam = OpenCvCameraFactory.getInstance()
-                .createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
-        org.man.c.SkystoneDetector detector = new org.man.c.SkystoneDetector(telemetry);
-        phoneCam.setPipeline(detector);
-        phoneCam.openCameraDeviceAsync(
-                () -> phoneCam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT)
-        );
-
-        waitForStart();
-        if (detector.getLocation() == One) {
-            sp = 1.0;
-
-        }
-        if(detector.getLocation()==Two)   {
-            sp=2.0;
-
-        }
-        else{
-            sp=0.5;
-        }
- Thread.sleep(1000);
-phoneCam.closeCameraDevice();
-        if (sp == 1.0) {
-
-        }
+    public void runOpMode() {
+        x = hardwareMap.get(DistanceSensor.class, "distance");
+        Rev2mDistanceSensor sensorTime = (Rev2mDistanceSensor) x;
+        robot.init(hardwareMap);
 
 
-        }
-    }
+           double sp = 0;
+            claw = hardwareMap.servo.get("claw");
+           leftDrive = hardwareMap.get(DcMotor.class, "FL");
+           rightDrive = hardwareMap.get(DcMotor.class, "FR");
+           leftBackDrive = hardwareMap.get(DcMotor.class, "BL");
+           rightBackDrive = hardwareMap.get(DcMotor.class, "BR");
+           int cameraMonitorViewId = hardwareMap.appContext
+                   .getResources().getIdentifier("cameraMonitorViewId",
+                           "id", hardwareMap.appContext.getPackageName());
+           phoneCam = OpenCvCameraFactory.getInstance()
+                   .createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+           org.man.c.SkystoneDetector detector = new org.man.c.SkystoneDetector(telemetry);
+           phoneCam.setPipeline(detector);
+           phoneCam.openCameraDeviceAsync(
+                   new OpenCvCamera.AsyncCameraOpenListener() {
+                       @Override
+                       public void onOpened() {
+                           phoneCam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+                       }
+                   }
+           );
+
+        telemetry.addData("RangeFront", x.getDistance(DistanceUnit.INCH));
+
+        telemetry.update();
+        robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Send telemetry message to indicate successful Encoder reset
+        telemetry.addData("Path0",  "Starting at %7d :%7d",
+                robot.leftDrive.getCurrentPosition(),
+                robot.rightDrive.getCurrentPosition());
+        telemetry.update();
+
+           waitForStart();
+           claw.setPosition(0.0);
+            sleep(1000);
+           phoneCam.closeCameraDevice();
+
+           if (detector.getLocation() == One) {
+           encoderDrive(.4,7.5,-7.5,3);
+            claw.setPosition(1.0);
+
+
+           }
+
+           if (detector.getLocation() == Zero) {
+
+               telemetry.addData("RangeFront", x.getDistance(DistanceUnit.INCH));
+
+               telemetry.update();
+               if(x.getDistance(DistanceUnit.INCH)<=47){
+
+                   telemetry.addData("RangeFront", x.getDistance(DistanceUnit.INCH));
+
+                   telemetry.update();
+                   arm.setPower(0.8);
+                   claw.setPosition(-0.1);
+                   leftBackDrive.setPower(0.7);
+                   rightDrive.setPower(0.7);
+                   rightBackDrive.setPower(0.7);
+                   leftDrive.setPower(0.7);
+
+               }
+               arm.setPower(-0.8);
+               sleep(500);
+               claw.setPosition(0.5);
+
+
+
+           }
+if(detector.getLocation() == Two){
+    leftDrive.setPower(2.0);
+    leftBackDrive.setPower(2.0);
+    rightBackDrive.setPower(2.0);
+
+    rightDrive.setPower(2.0);
+    sleep(1100);
+
 }
 
 
 
+
+
+
+
+
+
+
+       }
+
+
+    }
 
 
